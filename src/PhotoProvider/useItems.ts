a4a6a430470c1c61001052a74462b03de9394af5
wangjp <943375372@qq.com>
2022-03-17 23:47:48 +0800

    fix: 更新图片列表乱序问题

--- src/PhotoProvider/useItems.ts
+++ src/PhotoProvider/useItems.ts
@@ -10,12 +10,24 @@ type useItemsReturn = {
 export default function useItems(index: Ref<number>): useItemsReturn {
   const items = ref<ItemType[]>([]);
 
+  const getElementIndex = (children: HTMLCollection, child: HTMLElement | null | undefined) => {
+    return child ? Array.prototype.indexOf.call(children, child) : -1;
+  };
+
+  const sortItems = (items: ItemType[]) => {
+    const children = items[0].originRef?.parentNode?.children;
+    if (children && children.length) {
+      items.sort((cur, next) => getElementIndex(children, cur.originRef) - getElementIndex(children, next.originRef));
+    }
+  };
+
   const updateItem = (item: ItemType) => {
     const index = items.value.findIndex(({ key }) => item.key === key);
     if (index > -1) {
       items.value.splice(index, 1, item);
     } else {
       items.value.push(item);
+      sortItems(items.value);
     }
   };
 
